ae49829c81e88fec3263a8ecea12cbf19d36523a
jkim <junkyung_kim@brown.edu>
2018-10-29 17:32:21 -0400

    .

--- run_inference_jk.py
+++ run_inference_jk.py
@@ -1,8 +1,10 @@
 import sys
 import os
 import subprocess
+import numpy as np
 
 def write_custom_request(request_txt_fullpath, hdf_fullpath, ckpt_fullpath, output_fullpath,
+                         net_name,
                          fov_size, deltas,
                          image_mean=128, image_stddev=33):
     # 'validate_request_' + str(validate_jobid) + '.txt'
@@ -14,7 +16,7 @@ def write_custom_request(request_txt_fullpath, hdf_fullpath, ckpt_fullpath, outp
     file.write('image_stddev: ' + str(image_stddev) + '\n')
     file.write('seed_policy: "PolicyPeaks" \n')
     file.write('model_checkpoint_path: "' + ckpt_fullpath + '" \n')
-    file.write('model_name: "convstack_3d.ConvStack3DFFNModel" \n')
+    file.write('model_name: "'+net_name+'.ConvStack3DFFNModel" \n')
     file.write('model_args: "{\\"depth\\": 12, \\"fov_size\\": ' + str(fov_size) + ', \\"deltas\\": ' + str(deltas) + '}" \n')
     file.write('segmentation_output_dir: "' + output_fullpath + '" \n')
     file.write('inference_options { \n')
@@ -30,17 +32,23 @@ def write_custom_request(request_txt_fullpath, hdf_fullpath, ckpt_fullpath, outp
 
 if __name__ == '__main__':
 
+    num_machines = int(sys.argv[1])
+    i_machine = int(sys.argv[2])
+
     script_root = '/home/jk/PycharmProjects/ffn/'
     request_txt_root = os.path.join(script_root, 'configs')
-    hdf_root = '/media/data_cifs/connectomics/datasets/third_party/traditional/'
+    hdf_root = '/media/data_cifs/connectomics/datasets/third_party/'
     ckpt_root = '/media/data_cifs/connectomics/ffn_ckpts'
     output_root = '/media/data_cifs/connectomics/ffn_inferred'
 
-    net_name = 'ffn'
+    fov_type = 'traditional_fov'
     fov_size = [33, 33, 33] #[41, 41, 21] # [33,33,33]
     deltas = [8, 8, 8] #[10, 10, 5] #[8,8,8]
+
+    net_name = 'feedback_hgru_generic_longfb_3l'
+    train_dataset_name = 'neuroproof'
     dataset_name_list = ['neuroproof','cremi_a','cremi_b','cremi_c','berson','isbi2013']
-    dataset_type = 'val' #'train'
+    dataset_type = 'train' #'train'
     test_dataset_shape_list = [[520, 520, 520], #[250, 250, 250],
                                [1250, 625, 125],
                                [1250, 625, 125],
@@ -58,24 +66,33 @@ if __name__ == '__main__':
     image_mean = 128
     image_stddev = 33
 
-    for trian_dataset_name in dataset_name_list:
-        for test_dataset_name, test_dataset_shape in zip(dataset_name_list, test_dataset_shape_list):
-            for irep in range(num_model_repeats):
-                # cond_name = net_name + '_' + trian_dataset_name + '_r' + str(irep)
-                cond_name = net_name + '_' + 'pretrained' ############################################################### SWITCH
-                request_txt_fullpath = os.path.join(request_txt_root, cond_name + '_on_' + test_dataset_name + '.pbtxt')
-                hdf_fullpath = os.path.join(hdf_root, test_dataset_name, dataset_type, 'grayscale_maps.h5')
-                ckpt_fullpath = os.path.join(ckpt_root, cond_name, 'model.ckpt-27465036')
-                output_fullpath = os.path.join(output_root, cond_name, test_dataset_name)
+    kth_job = 0
+    for test_dataset_name, test_dataset_shape in zip(dataset_name_list, test_dataset_shape_list):
+        for irep in range(num_model_repeats):
+            kth_job += 1
+            if np.mod(kth_job, num_machines) != i_machine and num_machines != i_machine:
+                continue
+            elif np.mod(kth_job, num_machines) != 0 and num_machines == i_machine:
+                continue
 
-                write_custom_request(request_txt_fullpath, hdf_fullpath, ckpt_fullpath, output_fullpath,
-                                     fov_size, deltas,
-                                     image_mean, image_stddev)
+            net_cond_name = net_name + '_' + train_dataset_name + '_r' + str(irep)
+            ckpt_fullpath = os.path.join(ckpt_root, fov_type, net_cond_name, 'model.ckpt-40733')
 
-                command = 'python ' + os.path.join(script_root,'run_inference.py') + \
-                          ' --inference_request="$(cat ' + request_txt_fullpath + ')"' +\
-                          ' --bounding_box "start { x:0 y:0 z:0 } size { x:' + str(test_dataset_shape[0]) +' y:' + str(test_dataset_shape[1]) + ' z:' + str(test_dataset_shape[2]) + ' }"'
-                subprocess.call(command, shell=True)
+            request_txt_fullpath = os.path.join(request_txt_root, fov_type, net_cond_name + '_on_' + test_dataset_name + '_'+dataset_type+'.pbtxt')
+            hdf_fullpath = os.path.join(hdf_root, fov_type, test_dataset_name, dataset_type, 'grayscale_maps.h5')
+            output_fullpath = os.path.join(output_root, fov_type, net_cond_name, test_dataset_name, dataset_type)
+
+            #ckpt_fullpath = os.path.join(ckpt_root, fov_type, 'convstack_3d_pretrained/model.ckpt-27465036')
+
+            write_custom_request(request_txt_fullpath, hdf_fullpath, ckpt_fullpath, output_fullpath,
+                                 net_name,
+                                 fov_size, deltas,
+                                 image_mean, image_stddev)
+
+            command = 'python ' + os.path.join(script_root,'run_inference.py') + \
+                      ' --inference_request="$(cat ' + request_txt_fullpath + ')"' +\
+                      ' --bounding_box "start { x:0 y:0 z:0 } size { x:' + str(test_dataset_shape[0]) +' y:' + str(test_dataset_shape[1]) + ' z:' + str(test_dataset_shape[2]) + ' }"'
+            subprocess.call(command, shell=True)
 
 # .npz
 # #
--- train_jk.py
+++ train_jk.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     ckpt_root = os.path.join('/media/data_cifs/connectomics/ffn_ckpts', fov_type)
 
     load_from_ckpt = 'None'
-    #load_from_ckpt = os.path.join(ckpt_root, 'ffn_pretrained/model.ckpt-27465036') # THIS FEATURE DOESNT WORK
+    #load_from_ckpt = os.path.join(ckpt_root, 'convstack_3d_pretrained/model.ckpt-27465036') # THIS FEATURE DOESNT WORK
     #load_from_ckpt = os.path.join(ckpt_root, 'ffn_berson_r0/model.ckpt-0')
     num_model_repeats = 1
     max_steps = 64*100000/batch_size # 100K for ffn
@@ -61,7 +61,7 @@ if __name__ == '__main__':
             groundtruth_fullpath = os.path.join(hdf_root, dataset_name, dataset_type, 'groundtruth.h5')
             volume_fullpath = os.path.join(hdf_root, dataset_name, dataset_type, 'grayscale_maps.h5')
 
-            command = 'python ' + os.path.join(script_root, 'train_old.py') + \
+            command = 'python ' + os.path.join(script_root, 'train.py') + \
                       ' --train_coords ' + coords_fullpath + \
                       ' --data_volumes jk:' + volume_fullpath + ':raw' + \
                       ' --label_volumes jk:' + groundtruth_fullpath + ':stack' + \
@@ -74,6 +74,10 @@ if __name__ == '__main__':
                       ' --optimizer ' + optimizer + \
                       ' --load_from_ckpt ' + load_from_ckpt + \
                       ' --batch_size=' + str(batch_size)
+
+
+            ############# TODO(jk): USE DATA VOLUMES FOR MULTI VOLUME TRAINING????
+
 
             subprocess.call(command, shell=True)
 
