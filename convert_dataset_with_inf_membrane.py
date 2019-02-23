d4828ffddd805cdd7930d6e89e2475ee5049b6c4
Junkyung Kim <junkyungkim@Snowboard.local>
2019-02-23 18:49:52 -0500

    ,

--- convert_dataset_with_inf_membrane.py
+++ convert_dataset_with_inf_membrane.py
@@ -43,9 +43,9 @@ name = 'berson2x_w_inf_memb'
 volume = np.load('/media/data_cifs/connectomics/datasets/berson_v2_0.npz')['volume']
 membrane = np.load('/media/data_cifs/connectomics/datasets/berson_v2_predictions.npy')[:,:,:,0]
 membrane*=255
-# transpose
-membrane = np.transpose(membrane, (0,2,1))
-volume = np.transpose(volume, (0,2,1))
+# # transpose (DONT DO WITH NEW SEGMENT LABELS)
+# membrane = np.transpose(membrane, (0,2,1))
+# volume = np.transpose(volume, (0,2,1))
 membrane = np.expand_dims(membrane, axis=3)
 volume = np.expand_dims(volume, axis=3)
 volume_n_membrane = np.concatenate([volume, membrane], axis=3)
@@ -60,7 +60,6 @@ writer.close()
 # # BERSON 768 GT (because it needs to be redone)
 labelpath = '/media/data_cifs/connectomics/datasets/berson2x_labels.npy'
 gt_labels = np.load(labelpath)
-import ipdb;ipdb.set_trace()
 gt_labels, _, _ = skimage.segmentation.relabel_sequential(gt_labels, offset=1)
 writer = h5py.File(os.path.join(out_root,name,'train','groundtruth.h5'), 'w')
 writer.create_dataset('stack', data=gt_labels, dtype='<i8')
