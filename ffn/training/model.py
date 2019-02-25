d91865a9468ac2f5a926e28c77188a444cb09e4e
Junkyung Kim <junkyungkim@Snowboard.local>
2019-02-26 08:42:56 +0900

    .

--- ffn/training/model.py
+++ ffn/training/model.py
@@ -213,11 +213,18 @@ class FFNModel(object):
     dy = self.input_seed_size[1] - self.pred_mask_size[1]
     dz = self.input_seed_size[2] - self.pred_mask_size[2]
 
+    if self.optional_output_size is not None:
+      # crop output to fit optional output size
+      update = update[:,
+                      dz // 2: -(dz - dz // 2),
+                      dy // 2: -(dy - dy // 2),
+                      dx // 2: -(dx - dx // 2),
+                      :]
+
     if dx == 0 and dy == 0 and dz == 0:
       seed += update
     else:
-      import ipdb
-      ipdb.set_trace()
+      import ipdb;ipdb.set_trace()
       seed += tf.pad(update, [[0, 0],
                               [dz // 2, dz - dz // 2],
                               [dy // 2, dy - dy // 2],
--- ffn/training/models/htd_cnn_3l_in.py
+++ ffn/training/models/htd_cnn_3l_in.py
@@ -160,6 +160,7 @@ class ConvStack3DFFNModel(model.FFNModel):
   def __init__(self, with_membrane=False, fov_size=None, optional_output_size=None, deltas=None, batch_size=None, depth=9, is_training=True, adabn=False, reuse=False, tag='', TA=None):
     super(ConvStack3DFFNModel, self).__init__(deltas, batch_size, with_membrane, validation_mode=not(is_training), tag=tag)
 
+    self.optional_output_size = optional_output_size
     self.set_uniform_io_size(fov_size, optional_output_size=optional_output_size)
     self.depth = depth
     self.reuse=reuse
