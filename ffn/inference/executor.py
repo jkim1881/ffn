83f179bbb8b13aa3d888728d9262ef042d378e52
Junkyung Kim <junkyungkim@Snowboard.local>
2019-03-26 20:15:26 +0900

    fixed inference according to drew;s specs

--- ffn/inference/executor.py
+++ ffn/inference/executor.py
@@ -223,7 +223,7 @@ class ThreadingBatchExecutor(BatchExecutor):
         logit_shape = ret['logits'].shape
         # ret['logits'] = (ret['logits'] - ret['logits'].ravel().mean()) / (ret['logits'].std() + 1e-4)
         # ret['logits'] = gaussian(ret['logits'].squeeze().transpose(1, 2, 0), sigma=1, multichannel=True) #preserve_range=True, truncate=100)
-        ret['logits'] = ret['logits'].transpose(2, 0, 1).reshape(logit_shape)
+        # ret['logits'] = ret['logits'].transpose(2, 0, 1).reshape(logit_shape)
       except Exception as e:  # pylint:disable=broad-except
         logging.exception(e)
         # If calling TF didn't work (faulty hardware, misconfiguration, etc),
