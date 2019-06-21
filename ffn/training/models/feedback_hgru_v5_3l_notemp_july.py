f9d7553cc756930c2e63fb792e143f8d5db861c4
Junkyung Kim <junkyungkim@Snowboard.local>
2019-06-21 17:59:40 -0400

    .

--- ffn/training/models/feedback_hgru_v5_3l_notemp_july.py
+++ ffn/training/models/feedback_hgru_v5_3l_notemp_july.py
@@ -37,8 +37,9 @@ def _predict_object_mask(input_patches, input_seed, depth=9, is_training=True, a
         bn_decay = 0.95
     else:
         bn_decay = 1.0
+  else:
+    bn_decay = 0.95
   train_bn = True
-  # bn_decay = 1.0
 
   if input_patches.get_shape().as_list()[-1] == 2:
       image = tf.expand_dims(input_patches[:,:,:,:,0], axis=4)
